1c245da70eebb7e34f7be36a0ad3ce7c5c8e0585
Bruno C. Penha <brunopenha@users.noreply.github.com>
2026-02-13 16:35:37 +0100

    Update GreetingResource.java
    Shame....

--- src/main/java/br/nom/penha/bruno/GreetingResource.java
+++ src/main/java/br/nom/penha/bruno/GreetingResource.java
@@ -42,7 +42,7 @@ public class GreetingResource {
         if (number < 1) {
             return "Only natural numbers can be prime numbers.";
         }
-        if (number == 1 || number == 2 || number % 2 == 0) {
+        if (number % 2 == 0) {
             return number + " is not prime.";
         }
 
